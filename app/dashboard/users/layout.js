335b87101efa85cf5e5c4381d27cacf71d3fb0b5
codingba <codingbakelvin@gmail.com>
2024-03-01 01:25:17 +0000

    Create layout.js

---
+++ app/dashboard/users/layout.js
@@ -0,0 +1,16 @@
+
+
+const layout = ({children})=>{
+    return <html> 
+        <head>
+            <title>
+             Registered Users
+            </title>
+        </head>
+        <body>
+            <main>{children}</main>
+        </body>
+    </html> 
+} 
+
+export {layout as default}
